41a79d26656a315b17a0b11a0672c9b3887e05d7
darkucfknight <jordan.layton95@gmail.com>
2017-10-23 12:55:14 -0400

    redid cdn inclusion, added datatable
    lots of troubleshooting later, i added cdns that i think will be sufficient for each page.  also added datatable plugin cdns on index.html along with a sample datatable.

--- Website/scripts/index.js
+++ Website/scripts/index.js
@@ -5,3 +5,6 @@ $('#createButton').on('click', function() {
 $('#accountButton').on('click', function() {
 	window.location.href = 'account.html';
 });
+$('#dataTables-example').DataTable({
+	responsive: true
+});
