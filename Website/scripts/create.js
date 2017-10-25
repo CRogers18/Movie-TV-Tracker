b4c267e826ca33b0f6713d182f54e0ae10f0dd2a
darkucfknight <jordan.layton95@gmail.com>
2017-10-25 19:44:28 -0400

    Create connected to db and add edit skeleton
    Create now adds to the database and validates beforehand.
    Also added edit page skeleton that needs to be updated to display whichever index.html-selected show's data to display.

--- Website/scripts/create.js
+++ Website/scripts/create.js
@@ -1,3 +1,6 @@
+//variables
+var numRecords = 0;
+
 //navbar stuff
 $('#createButton').on('click', function() 
 {
@@ -8,12 +11,39 @@ $('#accountButton').on('click', function()
 	window.location.href = 'account.html';
 });
 
+//database connection
+var config = {
+	apiKey: "AIzaSyCt6z6XTg0HFZqKyyXhMDPicSYmgu_2XwM",
+	authDomain: "poosd-test.firebaseapp.com",
+	databaseURL: "https://poosd-test.firebaseio.com",
+	projectId: "poosd-test",
+	storageBucket: "poosd-test.appspot.com",
+	messagingSenderId: "1087046017322"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var mediaDbDataList = database.ref('media/');
+mediaDbDataList.on('child_added', function(data) 
+{
+	numRecords++;
+});
+
 //image upload 
 $(document).on('change', '.btn-file :file', function() 
 {
 	var input = $(this),
 		label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
 	input.trigger('fileselect', [label]);
+});
+
+$('#submitButton').on('click', function() 
+{
+	if(validateData())
+	{
+		addMedia();
+		window.location.href = 'index.html';
+	}
 });
 
 $('.btn-file :file').on('fileselect', function(event, label) 
@@ -28,7 +58,7 @@ $('.btn-file :file').on('fileselect', function(event, label)
 	{
 		if( log ) alert(log);
 	}
-    
+	
 });
 
 function readURL(input)
@@ -36,10 +66,10 @@ function readURL(input)
 	if (input.files && input.files[0]) 
 	{
 		var reader = new FileReader();
-	    reader.onload = function (e) 
-	    {
-	    	$('#img-upload').attr('src', e.target.result);
-	    }    
+		reader.onload = function (e) 
+		{
+			$('#img-upload').attr('src', e.target.result);
+		}
 	reader.readAsDataURL(input.files[0]);
 	}
 }
@@ -47,4 +77,64 @@ function readURL(input)
 $("#mediaImage").change(function()
 {
 	readURL(this);
-}); 	
+});
+
+function validateData()
+{
+	$('#inputErrorAlert').attr("hidden");
+	var isValid = true;
+	$("input, select, textarea").each(function() {
+		var element = $(this);
+		if (element.val() == "" || element.val() == null)
+		{
+			isValid = false;
+			$('#inputErrorAlert').removeAttr("hidden");
+			element.addClass("is-invalid");
+		}
+	});
+
+	return isValid;
+}
+
+
+function addMedia()
+{
+	var mediaId = numRecords+1;
+	var mediaName = document.getElementById('mediaName').value;
+	var mediaDescription = document.getElementById('mediaDescription').value;
+	var mediaReleaseDate = document.getElementById('mediaReleaseDate').value;
+	var mediaImage = document.getElementById('mediaImage').value;
+	var actualImageFile = document.getElementById('mediaImage').files[0];
+	var mediaCategory = document.getElementById('mediaCategory').value;
+	var mediaFormat = document.getElementById('mediaFormat').value;
+	var mediaUploader= "TODO"; //need to get user system figured out first
+	var mediaCast = document.getElementById('mediaCast').value;
+	var mediaTrailerLink = document.getElementById('mediaTrailerLink').value;
+	
+	var storageRef = firebase.storage().ref(mediaName);
+	storageRef.put(actualImageFile);
+	
+	addMediaToDB(mediaId, mediaName, mediaDescription, mediaReleaseDate, mediaImage, mediaCategory, mediaFormat, mediaUploader, mediaCast, mediaTrailerLink);
+}
+
+function addMediaToDB(mediaId, name, mediaDescription, mediaReleaseDate, mediaImage, mediaCategory, mediaFormat, mediaUploader, mediaCast, mediaTrailerLink){
+	var mediaRef = firebase.database().ref('media/'+name);
+	mediaRef.once('value')
+	.then(function(snapshot){
+		var exists = snapshot.exists();
+		if(!exists){
+			mediaRef.set({
+			   id: mediaId,
+			   title: name,
+			   description: mediaDescription,
+			   releaseDate: mediaReleaseDate,
+			   image: mediaImage,
+			   category: mediaCategory,
+			   format: mediaFormat,
+			   uploader: mediaUploader,
+			   cast: mediaCast,
+			   trailerLink: mediaTrailerLink
+		   });
+		}
+	});
+}
---
+++ Website/scripts/edit.js
@@ -0,0 +1,10 @@
+//navbar stuff
+$('#createButton').on('click', function() 
+{
+	window.location.href = 'create.html';
+});
+
+$('#accountButton').on('click', function() 
+{
+	window.location.href = 'account.html';
+});
