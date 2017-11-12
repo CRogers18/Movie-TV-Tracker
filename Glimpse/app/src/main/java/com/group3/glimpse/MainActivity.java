d53a6e555f4eaba7e8cb6504d4559f239c1feaf4
CRogers18 <c.rogers@knights.ucf.edu>
2017-11-12 18:37:05 -0500

    Main App Update 2
    + Login with Facebook and Google accounts added.
    + Small UI updates to login page
    * Latest build was made on Android Studio 3.0, if using older versions
    please update to 3.0 or higher or issues with gradle may occur.

--- Glimpse/app/src/main/java/com/group3/glimpse/MainActivity.java
+++ Glimpse/app/src/main/java/com/group3/glimpse/MainActivity.java
@@ -1,20 +1,42 @@
 package com.group3.glimpse;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth uAuth = FirebaseAuth.getInstance();
+    private CallbackManager cbm;
+    private GoogleApiClient gc;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +49,65 @@ public class MainActivity extends AppCompatActivity {
 
         Button newUserBtn = (Button) findViewById(R.id.newUser);
         Button loginBtn = (Button) findViewById(R.id.login);
+        LoginButton facebookBtn = (LoginButton) findViewById(R.id.fb_login);
+        SignInButton googleBtn = (SignInButton) findViewById(R.id.google_login);
+        googleBtn.setSize(SignInButton.SIZE_STANDARD);
 
-        newUserBtn.setOnClickListener(v -> newUserBtn.setVisibility(View.INVISIBLE));
+        // Callback manager for Facebook login
+        cbm = CallbackManager.Factory.create();
+
+        // What we ask users for from their Facebook accounts
+        facebookBtn.setReadPermissions("email", "public_profile");
+
+        // Callback to check that user login to facebook was successful
+        facebookBtn.registerCallback(cbm, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println("Facebook login attempt success! " + loginResult);
+
+                // Pass user credentials to firebase to ensure a new account is created in the
+                // database or the credentials are used to log in to an existing account
+                handleFBAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                System.out.println("Facebook login cancelled!");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                System.out.println("Facebook login error! " + error);
+            }
+        });
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Make a new API client
+        gc = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        System.out.println("Error connection failed to GoogleAPI!");
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        googleBtn.setOnClickListener(v -> googleSignIn());
+        googleBtn.setVisibility(View.INVISIBLE);
+        facebookBtn.setVisibility(View.INVISIBLE);
+
+        newUserBtn.setOnClickListener(v -> {
+            newUserBtn.setVisibility(View.INVISIBLE);
+            googleBtn.setVisibility(View.VISIBLE);
+            facebookBtn.setVisibility(View.VISIBLE);
+        });
+
         loginBtn.setOnClickListener(v -> {
 
             String user = username.getText().toString();
@@ -70,6 +149,74 @@ public class MainActivity extends AppCompatActivity {
         });
 
         username.setOnClickListener(v -> username.setText(""));
-
     }
+
+    private void googleSignIn()
+    {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(gc);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    // Handles passing info back to the Facebook SDK
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // If request is coming from google login
+        if (requestCode == RC_SIGN_IN)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (result.isSuccess())
+            {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount acc = result.getSignInAccount();
+                firebaseAuthWithGoogle(acc);
+            }
+
+            else
+                System.out.println("Google sign in succeeded!");
+        }
+
+        // Else request is from facebook
+        else
+            cbm.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // Connects Google+ logins with Firebase database users
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acc)
+    {
+        AuthCredential cred = GoogleAuthProvider.getCredential(acc.getIdToken(), null);
+
+        uAuth.signInWithCredential(cred).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                    // TO-DO: Switch intent to next activity
+                    System.out.println("User logged in to Glimpse w/ Google successfully!");
+                else
+                    System.out.println("User login to Glimpse w/ Google failed!");
+            }
+        });
+    }
+
+    // Connects Facebook logins with Firebase database users
+    private void handleFBAccessToken(AccessToken token)
+    {
+        System.out.println("Handling token: " + token);
+
+        AuthCredential cred = FacebookAuthProvider.getCredential(token.getToken());
+        uAuth.signInWithCredential(cred).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                    System.out.println("User signed in via Facebook successfully!");
+                    // TO-DO: change intent to loading screen activity
+                else
+                    System.out.println("Facebook login failed!");
+            }
+        });
+    }
+
 }
