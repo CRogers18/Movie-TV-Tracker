e3d232a790b4486b1083a4d37503970619e886bf
darkucfknight <jordan.layton95@gmail.com>
2017-11-23 01:13:39 -0500

    Account page (incomplete)
    started basis for an account page. will add account image and hopefully image changer to left side once that feature is available.

--- Website/scripts/account.js
+++ Website/scripts/account.js
@@ -1,3 +1,5 @@
+var numMovies = 0;
+var numShows = 0;
 //navbar stuff
 $('#createButton').on('click', function() 
 {
@@ -12,3 +14,51 @@ $('#logoffButton').on('click', function()
 {
     window.location.href = 'login.html';
 });
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCt6z6XTg0HFZqKyyXhMDPicSYmgu_2XwM",
+	authDomain: "poosd-test.firebaseapp.com",
+	databaseURL: "https://poosd-test.firebaseio.com",
+	projectId: "poosd-test",
+	storageBucket: "poosd-test.appspot.com",
+	messagingSenderId: "1087046017322"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var mediaDbDataList = database.ref('media/');
+
+//user stuff
+firebase.auth().onAuthStateChanged(function(user){
+	if(user){
+		var email = user.email;
+		var uid = user.uid;
+
+		console.log(email+" "+" "+uid);
+		$('#username').val(email);
+		getStats(email);
+	}else{
+		console.log("not signed in");
+	}
+})
+
+function getStats(email)
+{
+	mediaDbDataList.on('child_added', function(data) 
+	{
+	    var releaseDateTemp = new Date(0);
+	    releaseDateTemp.setUTCSeconds(data.val().releaseDate);
+	    var releaseDateStandard = (releaseDateTemp.getMonth() + 1) + '/' + releaseDateTemp.getDate() + '/' +  releaseDateTemp.getFullYear();
+	    if (data.val().uploader === email && data.val().format === 'Movie')
+	    {
+		   numMovies++;
+		   $('#moviesAdded').val(numMovies);
+	    }
+	    if (data.val().uploader === email && data.val().format === 'TV Show')
+	    {
+		   numShows++;
+		   $('#showsAdded').val(numShows);
+	    }
+	});
+}
+
